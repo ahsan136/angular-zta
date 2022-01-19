7b936e899f47081a008777b8bdcb64f3dccaa9f1
toridenney <tori.denney@att.net>
2022-01-18 21:15:43 -0700

    programmed 3 demo scenarios

--- src/app/flowchart/flowchart.component.ts
+++ src/app/flowchart/flowchart.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-//import { geometry, Image, Surface, Path, Text, Group } from '@progress/kendo-drawing';
-//const { Rect, Point, Size, transform } = geometry;
-// import {MatButtonToggleChange} from '@angular/material/button-toggle';
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 export interface Tile {
   color: string;
@@ -17,99 +15,115 @@ export interface Tile {
 })
 export class FlowchartComponent implements OnInit {
 
-  constructor() {
+  constructor(public dialog: MatDialog) {
     
-   }
+  }
 
-  ngOnInit(): boolean {
-   
-return this.updateVal();
-    }
-    
-  tiles: Tile[] = [
-    {text: 'One', cols: 3, rows: 3, color: 'blue'},
-    {text: 'Two', cols: 1, rows: 4, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightgreen'},
-    {text: 'Four', cols: 2, rows: 1, color: 'blue'},
-  ];
+  ngOnInit(){
 
-updateVal(){
-  if (this.val1 == true){
-    return this.val3=false;
-  }else if( this.val1 == false){
-    return this.val3=true
-  }  
-  // if(this.val1==false){
-  //   return this.val3=true
-  // }
-}
-
-  // status: string;
-  // enableDisableRule2() {
-  //   if(this.val2==true){
-  //     return this.status=true
-  //   }else if(this.val2==false){
-  //     return this.status=false
-  //   }
-  // } 
+  }
 
   val1:boolean = true; 
-  val2:boolean = true;
-  val3:boolean = false;  
+  val2:boolean = true; 
+  val3:boolean = true; 
+
+  onLeakDetected(value){
+    this.openLeakModal();
+
+    this.val1 = false;
+    this.val3 = true;
+    return this.val2=true
+  }
+
+  onNetworkSwitch(value){
+    this.val1 = false;
+    this.val2 = false;
+    this.val3 = false;
+
+    this.openNetworkModal();
+
+    setTimeout(this.resetSystem, 4000);
+  }
+
+  resetSystem(){
+    this.val1 = true;
+    this.val2 = true;
+    this.val3 = true;
+    console.log("4 sec");
+  }
+
+  onBadActor(value){
+    this.openBadActorModal();
+    this.val1 = true;
+    this.val3 = true;
+    return this.val2=false
+  }
   
-  getClassOfVal1(val1) {
-    if (val1 == false) {
-      return 'valve_img_red';
-    } else if (val1 ==true) {
-      return 'valve_img';
-    } 
+  openLeakModal() {
+    this.dialog.open(ModalDataDialog, {
+      data: {
+        error: 'Leak discovered.',
+        message: 'Rerouted water flow ',
+      },
+    });
   }
 
-  getClassOfVal2(val2) {
-    if (val2 == false) {
-      return 'valve_img_red';
-    } else if (val2 ==true) {
-      return 'valve_img';
-    } 
-    
+  openNetworkModal() {
+    this.dialog.open(ModalDataDialog, {
+      data: {
+        error: 'Network failure.',
+        message: 'Switched network connection from terrestrial to space ',
+      },
+    });
   }
 
-  getClassOfVal3(val3) {
-    if (val3 == false) {
-      return 'valve_img_red_2';
-    } else if (val3 ==true) {
-      return 'valve_img_2';
-    } 
+  openBadActorModal() {
+    this.dialog.open(ModalDataDialog, {
+      data: {
+        error: 'Bad actor recognized.',
+        message: 'The infiltrated valve was blocked and the flow was redirected ',
+      },
+    });
   }
 
-  getval1(val1){
-    if(val1==true){
+  getval1(){
+    if(this.val1==true){
       return 100;
     }
-    else if(val1==false){
+    else if(this.val1==false){
       return 0;
     }
   }
 
-  getval2(val2){
-    if(val2==true){
+  getval2(){
+    if(this.val2==true){
       return 100;
     }
-    else if(val2==false){
+    else if(this.val2==false){
       return 0;
     }
   }
 
-  getval3(val3){
-    if(val3==true){
+  getval3(){
+    if(this.val3==true){
       return 100;
     }
-    else if(val3==false){
+    else if(this.val3==false){
       return 0;
     }
   }
 
-  // toggleView(change: MatButtonToggleChange){
-  //   this.toggle = change.value;
-  // }
+}
+
+export interface ModalData {
+  error: "";
+  message: "";
+}
+
+@Component({
+  selector: 'modal-data-dialog',
+  templateUrl: 'popupmodal.html'
+})
+export class ModalDataDialog {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: ModalData) {}
 }
